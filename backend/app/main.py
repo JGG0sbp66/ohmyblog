b4c35580dbcab42e2b2cbd01e40c6e3857ba2f23
JGG0sbp66 <2791456537@qq.com>
2025-10-07 20:00:58 +0800

    新增main函数注册中间件

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,11 @@
 from fastapi_offline import FastAPIOffline
 from app.utils import lifespan
+from app.core.middleware import setup_middleware
 
 app = FastAPIOffline(lifespan=lifespan)
+
+# 注册中间件
+setup_middleware(app)
 
 
 @app.get("/")
