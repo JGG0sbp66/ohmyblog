cc978745adf904b24b60c2d42bf077f897aae996
JGG0sbp66 <2791456537@qq.com>
2025-12-14 14:35:35 +0800

    优化主题颜色映射，新增深色模式支持

--- src/views/show/index.vue
+++ src/views/show/index.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+import ToggleColor from '@/components/base/toggleColor.vue';
+import Loading from '@/components/base/loading.vue';
+import Button from '@/components/button/button.vue';
+</script>
+
 <template>
-    <div>show page</div>
+    <div class="ml-10 mt-10 h-10 w-32"><Button :isloading="true"><Loading :colorClass="'text-white'" /></Button></div>
+    <Loading class="ml-10 mt-10" :size-class="'w-10 h-10'"/>
+    <ToggleColor />
 </template>
