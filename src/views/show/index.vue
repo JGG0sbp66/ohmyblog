676089cfdeed6b23c5f59f0d0c505b2f27e128ca
JGG0sbp66 <2791456537@qq.com>
2025-12-16 18:25:14 +0800

    移除未使用的ButtonSecondary和ColorPicker组件，优化代码结构

--- src/views/show/index.vue
+++ src/views/show/index.vue
@@ -4,8 +4,7 @@ import ToggleColor from '@/components/theme/ToggleColor.vue';
 import ToggleTheme from '@/components/theme/ToggleTheme.vue';
 import Loading from '@/components/icon/Loading.vue';
 import ButtonPrimary from '@/components/base/button/ButtonPrimary.vue';
-import ButtonSecondary from '@/components/base/button/ButtonSecondary.vue';
-import ColorPicker from '@/components/icon/ColorPicker.vue';
+
 const isloading = ref(false);
 const text = ref('下一步');
 
@@ -20,13 +19,14 @@ const testLoading = () => {
 </script>
 
 <template>
-    <div class="ml-10 mt-10 h-10 w-32"><ButtonPrimary @click="testLoading" :isLoading="isloading" :text="text"><Loading /></ButtonPrimary></div>
-    <Loading class="ml-10 mt-10" :size-class="'w-10 h-10'" :colorClass="'text-primary'"/>
+  <div class="ml-10 mt-10 h-10 w-32">
+    <ButtonPrimary @click="testLoading" :isLoading="isloading" :text="text">
+      <Loading />
+    </ButtonPrimary>
+  </div>
+  <Loading class="ml-10 mt-10" :size-class="'w-10 h-10'" :colorClass="'text-primary'" />
+  <ToggleTheme class="mt-10 ml-10" />
+  <div class="bg-bg-card h-18 flex items-center justify-center">
     <ToggleColor />
-    <ToggleTheme class="mt-10 ml-10"/>
-    <div class="bg-bg-card h-18 flex items-center justify-center">
-      <div class="w-11 h-11">
-      <ButtonSecondary :hasSlot="true"><ColorPicker /></ButtonSecondary>
-    </div>
-    </div>
+  </div>
 </template>
