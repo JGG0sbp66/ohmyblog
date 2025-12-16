3acf775999b2f8239445dffedda54bf2718f6536
JGG0sbp66 <2791456537@qq.com>
2025-12-16 18:25:10 +0800

    重构ToggleColor组件，优化显示逻辑和样式，新增点击外部关闭功能

--- src/components/theme/toggleColor.vue
+++ src/components/theme/toggleColor.vue
@@ -1,17 +1,48 @@
 <script lang="ts" setup>
 import { themeOptions, changeBrandColor, isDark } from './useTheme';
+import ButtonSecondary from '@/components/base/button/ButtonSecondary.vue';
+import ColorPicker from '@/components/icon/ColorPicker.vue';
+import { ref } from 'vue';
+import { onClickOutside } from '@vueuse/core';
+
+const isShow = ref(false);
+const containerRef = ref(null)
+
+const toggleShow = () => {
+    isShow.value = !isShow.value;
+};
+
+onClickOutside(containerRef, () => {
+    isShow.value = false;
+});
+
+const positionClass = `absolute top-full z-50`
+
 </script>
 
 <template>
-    <div class="flex gap-2">
-        <button 
-            v-for="item in themeOptions" 
-            :key="item.name" 
-            @click="changeBrandColor(item.name)"
-            class="w-8 h-8 rounded-full shadow-sm"
-            :style="{ backgroundColor: isDark ? item.dark.base : item.light.base }"
-            :title="item.name"
-        >
-        </button>
+    <div class="relative" ref="containerRef">
+        <div class="w-11 h-11">
+            <!-- TODO: 点击后，hover样式持续显示 -->
+            <ButtonSecondary :hasSlot="true" @click="toggleShow">
+                <ColorPicker />
+            </ButtonSecondary>
+        </div>
+
+        <!-- 画板显示区域 -->
+        <!-- TODO: 1. 新增标题显示
+                   2. 新增选中状态显示 -->
+        <Transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-2"
+            enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150"
+            leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-2">
+            <div v-if="isShow" :class="['bg-bg-card', positionClass, 'mt-6', 'p-4', 'rounded-lg', 'shadow-lg']">
+                <div class="flex gap-2">
+                    <button v-for="item in themeOptions" :key="item.name" @click="changeBrandColor(item.name)"
+                        class="w-8 h-8 rounded-full shadow-sm"
+                        :style="{ backgroundColor: isDark ? item.dark.base : item.light.base }" :title="item.name">
+                    </button>
+                </div>
+            </div>
+        </Transition>
     </div>
 </template>
