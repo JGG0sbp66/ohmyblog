911a204993d233a625c8fd4a8a8818bafbfc8aef
JGG0sbp66 <2791456537@qq.com>
2025-12-17 21:22:16 +0800

    添加主题切换按钮，优化SVG切换显示

--- src/components/theme/toggleTheme.vue
+++ src/components/theme/toggleTheme.vue
@@ -1,8 +1,12 @@
 <script lang="ts" setup>
 import { isDark, toggleDarkMode } from './useTheme';
+import ButtonSecondary from '../base/button/ButtonSecondary.vue';
+import ThemePicker from '../icon/ThemePicker.vue';
 </script>
 
 <template>
+    <!-- 后面再优化svg切换 -->
+    <ButtonSecondary :hasSlot="true" @click="toggleDarkMode"><ThemePicker :theme="'system'"/></ButtonSecondary>
     <button @click="toggleDarkMode()" class="p-2 border rounded">
         {{ isDark ? '🌙 关灯' : '🌞 开灯' }}
     </button>
