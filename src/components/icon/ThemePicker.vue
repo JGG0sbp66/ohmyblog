2b8009a61612b03c62cdfef0b5e0bad03336ad06
JGG0sbp66 <2791456537@qq.com>
2025-12-18 21:21:22 +0800

    修复ThemePicker组件中的图标导入错误，确保系统主题图标正确显示

--- src/components/icon/ThemePicker.vue
+++ src/components/icon/ThemePicker.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
-import { RiContrast2Line, RiSunLine, RiContractLine } from '@remixicon/vue';
+import { RiContrast2Line, RiSunLine, RiContrastLine } from '@remixicon/vue';
 const props = defineProps<{
     theme: string
 }>()
 </script>
 
 <template>
-    <RiContractLine v-if="props.theme == 'system'" class="text-[1.25rem]" />
+    <RiContrastLine v-if="props.theme == 'system'" class="text-[1.25rem]" />
     <RiSunLine v-if="props.theme == 'light'" class="text-[1.25rem]" />
     <RiContrast2Line v-if="props.theme == 'dark'" class="text-[1.25rem]" />
 </template>
