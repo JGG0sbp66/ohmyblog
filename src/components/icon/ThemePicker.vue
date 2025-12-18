9b3ff252ffc14f26c219015423cc3945cb0df67c
JGG0sbp66 <2791456537@qq.com>
2025-12-18 21:03:06 +0800

    修复ThemePicker组件中系统主题图标的引用，使用RiContractLine替代RiContrast2Line

--- src/components/icon/ThemePicker.vue
+++ src/components/icon/ThemePicker.vue
@@ -6,7 +6,7 @@ const props = defineProps<{
 </script>
 
 <template>
-    <RiContrast2Line v-if="props.theme == 'system'" class="text-[1.25rem]" />
+    <RiContractLine v-if="props.theme == 'system'" class="text-[1.25rem]" />
     <RiSunLine v-if="props.theme == 'light'" class="text-[1.25rem]" />
     <RiContrast2Line v-if="props.theme == 'dark'" class="text-[1.25rem]" />
 </template>
