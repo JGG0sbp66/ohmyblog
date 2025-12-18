3c58092169d54f38d1586a8d9902f0a2c304b109
JGG0sbp66 <2791456537@qq.com>
2025-12-18 21:06:54 +0800

    新增TODO

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -10,7 +10,8 @@ const messages = {
 
 // 创建 i18n 实例
 // TODO: 1. 实现将语言存入 localStorage 并在初始化时读取
-//       2. 实现根据浏览器语言自动读取默认语言
+// TODO: 2. 实现根据浏览器语言自动读取默认语言
+// TODO: 3. 将ts语言库修改为json格式
 const i18n = createI18n({
     // 为了更好的 TS 支持和 Vue3 特性，通常建议设为 false
     // 这里我们使用 legacy: false 以便更好地配合 Composition API
