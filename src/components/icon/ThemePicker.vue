9bd05c15a6d709fa74f1adca13e610ba5e8aeb54
JGG0sbp66 <2791456537@qq.com>
2025-12-19 20:42:48 +0800

    修复ThemePicker组件中的图标导入错误，确保深色模式下正确显示月亮图标

--- src/components/icon/ThemePicker.vue
+++ src/components/icon/ThemePicker.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { RiContrast2Line, RiSunLine, RiContrastLine } from '@remixicon/vue';
+import { RiContrastLine, RiSunLine, RiMoonLine } from '@remixicon/vue';
 const props = defineProps<{
     theme: string
 }>()
@@ -8,5 +8,5 @@ const props = defineProps<{
 <template>
     <RiContrastLine v-if="props.theme == 'system'" class="text-[1.25rem]" />
     <RiSunLine v-if="props.theme == 'light'" class="text-[1.25rem]" />
-    <RiContrast2Line v-if="props.theme == 'dark'" class="text-[1.25rem]" />
+    <RiMoonLine v-if="props.theme == 'dark'" class="text-[1.25rem]" />
 </template>
