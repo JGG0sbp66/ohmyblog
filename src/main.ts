4be8a6f72217b1a110d87ef73ee5ad916dd1fe0c
JGG0sbp66 <2791456537@qq.com>
2025-12-19 21:14:51 +0800

    更新本地化逻辑，导入新的语言包路径

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import i18n from './locales'
+import i18n from './locales/useLang'
 
 // 全局引入 Tailwind CSS 样式
 import './css/tailwind.css'
