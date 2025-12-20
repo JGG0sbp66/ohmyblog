717e394a4aba00f778cb039a6ab83b634c1658de
JGG0sbp66 <2791456537@qq.com>
2025-12-20 20:21:24 +0800

    修复BasePop组件，移除弹窗内边距以优化布局

--- src/components/base/pop/BasePop.vue
+++ src/components/base/pop/BasePop.vue
@@ -29,7 +29,7 @@ onClickOutside(panelRef, (event) => {
     <Transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-2"
         enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150"
         leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-2">
-        <div v-if="modelValue" ref="panelRef" class="bg-bg-card absolute z-50 mt-6 p-4 rounded-lg shadow-lg"
+        <div v-if="modelValue" ref="panelRef" class="bg-bg-card absolute z-50 mt-6 rounded-lg shadow-lg"
             :class="$attrs.class">
             <slot></slot>
         </div>
