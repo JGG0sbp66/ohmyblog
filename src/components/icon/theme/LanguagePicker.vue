d0b4eaf6d670da48b0378a8806732000f3e73c92
JGG0sbp66 <2791456537@qq.com>
2025-12-22 21:05:46 +0800

    优化语言选择组件，添加语言属性并恢复条件渲染逻辑

--- src/components/icon/theme/LanguagePicker.vue
+++ src/components/icon/theme/LanguagePicker.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup>
 import { RiTranslate, RiEmphasisCn, RiEnglishInput } from '@remixicon/vue';
+const props = defineProps<{
+    language: string
+}>()
 </script>
 
 <template>
-    <RiTranslate class="text-[1.25rem] w-[1em] h-[1em]" />
-    <!-- <RiEmphasisCn class="text-[1.25rem] w-[1em] h-[1em]" /> -->
-    <!-- <RiEnglishInput class="text-[1.25rem] w-[1em] h-[1em]" /> -->
+    <RiTranslate v-if="props.language == 'translate'" class="text-[1.25rem] w-[1em] h-[1em]" />
+    <RiEmphasisCn v-if="props.language == 'zh-CN'" class="text-[1.25rem] w-[1em] h-[1em]" />
+    <RiEnglishInput v-if="props.language == 'en-US'" class="text-[1.25rem] w-[1em] h-[1em]" />
 </template>
