25064f1f72caf3896800c267c780d5cf9064afb2
JGG0sbp66 <2791456537@qq.com>
2025-12-22 20:59:40 +0800

    优化主题切换组件，添加国际化支持并调整主题选项的映射逻辑

--- src/components/theme/toggleTheme.vue
+++ src/components/theme/toggleTheme.vue
@@ -3,7 +3,17 @@ import { colorMode, cycleTheme, THEME_MODES, setTheme } from './useTheme';
 import ButtonSecondary from '../base/button/ButtonSecondary.vue';
 import ThemePicker from '../icon/theme/ThemePicker.vue';
 import BasePop from '../base/pop/BasePop.vue';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n(); 
+
+const themeOptions = computed(() => {
+  return THEME_MODES.map((mode) => ({
+    value: mode,
+    label: t(`components.theme.ToggleTheme.${mode}`) 
+  }));
+});
 
 const isShow = ref(false);
 const btnRef = ref(null);
@@ -32,13 +42,13 @@ const isActive = (value: string) => {
         </div>
 
         <!-- 桥接层：填充按钮和浮窗之间的间隙，防止鼠标移动时浮窗消失 -->
-        <div v-if="isShow" class="absolute left-0 right-0 h-6 top-11"></div>
+        <div v-if="isShow" class="absolute -left-5 right-0 h-6 w-36 top-11"></div>
 
         <BasePop v-model="isShow" :trigger-ref="btnRef" class="min-w-30 p-2">
-            <div v-for="value in THEME_MODES" :key="value" class="flex flex-col mt-0.5 mb-0.5">
+            <div v-for="option in themeOptions" :key="option.value" class="flex flex-col mt-0.5 mb-0.5">
                 <!-- TODO: 1. 将value映射为可读性更好的名称，并做i18n处理 -->
-                <ButtonSecondary @click="setTheme(value)" :text="value" class="p-2 justify-start px-3" :isActive="isActive(value)" :hasSlot="true">
-                    <ThemePicker :theme="value" />
+                <ButtonSecondary @click="setTheme(option.value)" :text="option.label" class="p-2 justify-start px-3" :isActive="isActive(option.value)" :hasSlot="true">
+                    <ThemePicker :theme="option.value" />
                 </ButtonSecondary>
             </div>
         </BasePop>
