3ec1e16387add3e76078c62a4933a2cb3cc2f19c
JGG0sbp66 <2791456537@qq.com>
2025-12-24 18:12:20 +0800

    优化语言切换

--- src/components/theme/ToggleLanguage.vue
+++ src/components/theme/ToggleLanguage.vue
@@ -1,16 +1,9 @@
 <script lang="ts" setup>
 import ButtonSecondary from '@/components/base/button/ButtonSecondary.vue';
 import LanguagePicker from '../icon/theme/LanguagePicker.vue'
-import BasePop from '../base/pop/BasePop.vue';
-import { ref } from 'vue';
-import i18n,{ SUPPORTED_LOCALES, setLocale, type LocaleType } from '@/locales/useLang';
+import i18n, { SUPPORTED_LOCALES, setLocale, type LocaleType } from '@/locales/useLang';
+import DropButton from '../common/button/DropButton.vue';
 
-const isShow = ref(false);
-const btnRef = ref(null);
-
-const toggleShow = () => {
-    isShow.value = !isShow.value;
-};
 
 // 判断当前语言是否为选中语言
 const isActive = (value: string) => {
@@ -24,18 +17,20 @@ const switchLanguage = (value: LocaleType) => {
 </script>
 
 <template>
-    <div class="relative" ref="btnRef">
-        <div class="w-11 h-11">
-            <ButtonSecondary :hasSlot="true" :isActive="isShow" @click="toggleShow">
-                <LanguagePicker :language="'translate'"/>
+    <DropButton :contentClass="'min-w-32 p-2'">
+        <template #trigger="{ active }">
+            <ButtonSecondary :hasSlot="true" :isActive="active">
+                <LanguagePicker :language="'translate'" />
             </ButtonSecondary>
-        </div>
-        <BasePop v-model="isShow" :trigger-ref="btnRef" class="min-w-32 p-2">
+        </template>
+
+        <template #content>
             <div v-for="value in SUPPORTED_LOCALES" :key="value.label" class="flex flex-col mt-0.5 mb-0.5">
-                <ButtonSecondary @click="switchLanguage(value.value)" :text="value.label" class="p-2" :isActive="isActive(value.value)" :hasSlot="true">
-                    <LanguagePicker :language="value.value"/>
+                <ButtonSecondary @click="switchLanguage(value.value)" :text="value.label" class="p-2"
+                    :isActive="isActive(value.value)" :hasSlot="true">
+                    <LanguagePicker :language="value.value" />
                 </ButtonSecondary>
             </div>
-        </BasePop>
-    </div>
+        </template>
+    </DropButton>
 </template>
