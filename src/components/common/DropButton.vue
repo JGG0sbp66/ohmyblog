f36fae678f6db7ca396eb4dbf48a7ec7445816b2
JGG0sbp66 <2791456537@qq.com>
2025-12-24 18:05:55 +0800

    新增DropButton组件，支持鼠标悬停显示弹窗内容

---
+++ src/components/common/DropButton.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import BasePop from '../base/pop/BasePop.vue';
+import { ref } from 'vue';
+
+interface Props {
+    triggerClass?: string;
+    contentClass?: string;
+}
+
+const {
+    triggerClass = 'w-11 h-11',
+    contentClass = 'min-w-30 p-2'
+} = defineProps<Props>();
+
+const isShow = ref(false);
+const btnRef = ref(null);
+
+const showPop = () => {
+    isShow.value = true;
+};
+
+const hidePop = () => {
+    isShow.value = false;
+};
+</script>
+
+<template>
+    <div class="relative" ref="btnRef" @mouseenter="showPop" @mouseleave="hidePop">
+        <div :class="triggerClass">
+            <slot name="trigger" :active="isShow"></slot>
+        </div>
+
+        <!-- 桥接层：填充按钮和浮窗之间的间隙，防止鼠标移动时浮窗消失 -->
+        <div v-if="isShow" class="absolute -left-5 right-0 h-6 w-36 top-11"></div>
+
+        <BasePop v-model="isShow" :trigger-ref="btnRef" :class="contentClass">
+            <slot name="content"></slot>
+        </BasePop>
+    </div>
+</template>
