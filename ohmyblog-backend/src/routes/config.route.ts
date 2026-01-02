ea282a45ac9c3a6fe5ad01ffd0031692fcef9e7a
JGG0sbp66 <2791456537@qq.com>
2026-01-02 20:48:58 +0800

    feat: 添加获取单个配置的接口，支持根据配置键获取配置

--- ohmyblog-backend/src/routes/config.route.ts
+++ ohmyblog-backend/src/routes/config.route.ts
@@ -2,7 +2,7 @@
 import { Elysia } from "elysia";
 import { configService } from "../services/config.service";
 import { ConfigUpsertDTO } from "../dtos/config.dto";
-import { authPlugin } from "../plugins/auth.plugin";
+import { authPlugin, type JwtUserPayload } from "../plugins/auth.plugin";
 import { ensureAdminIfExists } from "../plugins/adminGuard";
 
 // TODO: 后续再新增删除，查找接口，目前只支持更新
@@ -28,5 +28,32 @@ export const configRoute = new Elysia({ name: "configRoute" })
                     beforeHandle: ensureAdminIfExists,
                     detail: { summary: "创建或更新配置（POST）" },
                     body: ConfigUpsertDTO,
-                }),
+                })
+                /**
+                 * GET /config/:configKey
+                 * - 获取单个配置
+                 */
+                .get(
+                    "/:configKey",
+                    async (
+                        { params, user }: {
+                            params: { configKey: string };
+                            user?: JwtUserPayload;
+                        },
+                    ) => {
+                        const isAdmin = user?.role === "admin";
+
+                        const config = await configService.getByKey(
+                            params.configKey,
+                            isAdmin,
+                        );
+                        return {
+                            message: "获取成功",
+                            config,
+                        };
+                    },
+                    {
+                        detail: { summary: "获取单个配置（GET）" },
+                    },
+                ),
     );
