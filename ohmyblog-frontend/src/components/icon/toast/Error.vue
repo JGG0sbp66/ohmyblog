00a248ecf1e705d8fe44ca12c08c645b29c2d6ce
JGG0sbp66 <2791456537@qq.com>
2026-01-04 21:19:14 +0800

    feat: 新增 Error.vue 组件以显示错误通知图标

---
+++ ohmyblog-frontend/src/components/icon/toast/Error.vue
@@ -0,0 +1,8 @@
+<!-- src/components/icon/toast/Error.vue -->
+<script setup lang="ts">
+import { RiCloseCircleFill } from '@remixicon/vue';
+</script>
+
+<template>
+    <RiCloseCircleFill class="text-[1.25rem] w-[1em] h-[1em]" />
+</template>
