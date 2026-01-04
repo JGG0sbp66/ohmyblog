0c67c118c812ecfdfb45bc5829cc4c3b6dd63c36
JGG0sbp66 <2791456537@qq.com>
2026-01-04 21:18:04 +0800

    feat: 新增 Info.vue 组件以显示信息通知图标

---
+++ ohmyblog-frontend/src/components/icon/toast/Info.vue
@@ -0,0 +1,8 @@
+<!-- src/components/icon/toast/Info.vue -->
+<script setup lang="ts">
+import { RiInformationFill } from '@remixicon/vue';
+</script>
+
+<template>
+    <RiInformationFill class="text-[1.25rem] w-[1em] h-[1em]" />
+</template>
