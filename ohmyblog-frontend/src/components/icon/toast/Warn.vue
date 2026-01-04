7a1ad931bf5f36d8368896334c10e0e7cf4c23a4
JGG0sbp66 <2791456537@qq.com>
2026-01-04 21:17:36 +0800

    feat: 新增 Warn.vue 组件以显示警告通知图标

---
+++ ohmyblog-frontend/src/components/icon/toast/Warn.vue
@@ -0,0 +1,8 @@
+<!-- src/components/icon/toast/Warn.vue -->
+<script setup lang="ts">
+import { RiAlarmWarningFill } from '@remixicon/vue';
+</script>
+
+<template>
+    <RiAlarmWarningFill class="text-[1.25rem] w-[1em] h-[1em]" />
+</template>
