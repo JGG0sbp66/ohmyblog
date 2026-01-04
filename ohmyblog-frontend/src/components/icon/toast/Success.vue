c0ec481ddb3e5a4a4086263f9628746ed6be86ef
JGG0sbp66 <2791456537@qq.com>
2026-01-04 21:13:21 +0800

    feat: 新增 Success.vue 组件以显示成功通知图标

---
+++ ohmyblog-frontend/src/components/icon/toast/Success.vue
@@ -0,0 +1,8 @@
+<!-- src/components/icon/toast/Success.vue -->
+<script setup lang="ts">
+import { RiCheckboxCircleFill } from '@remixicon/vue';
+</script>
+
+<template>
+    <RiCheckboxCircleFill class="text-[1.25rem] w-[1em] h-[1em]" />
+</template>
