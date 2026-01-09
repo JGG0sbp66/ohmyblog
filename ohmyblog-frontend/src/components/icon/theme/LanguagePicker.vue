b12bde803d7cd9c0330f5429f1fbd3b55b2d1236
JGG0sbp66 <2791456537@qq.com>
2026-01-09 21:39:23 +0800

    feat: 添加 RiTranslate 图标导入到 LanguagePicker 组件

--- ohmyblog-frontend/src/components/icon/theme/LanguagePicker.vue
+++ ohmyblog-frontend/src/components/icon/theme/LanguagePicker.vue
@@ -1,13 +1,13 @@
 <!-- src/components/icon/theme/LanguagePicker.vue -->
 <script lang="ts" setup>
-import { RiEmphasisCn, RiEnglishInput } from '@remixicon/vue';
+import { RiTranslate, RiEmphasisCn, RiEnglishInput } from '@remixicon/vue';
 const props = defineProps<{
     language: string
 }>()
 </script>
 
 <template>
-     <RiTranslate v-if="props.language == 'translate'" class="w-5 h-5" />
+    <RiTranslate v-if="props.language == 'translate'" class="w-5 h-5" />
     <RiEmphasisCn v-if="props.language == 'zh-CN'" class="w-5 h-5" />
     <RiEnglishInput v-if="props.language == 'en-US'" class="w-5 h-5" />
 </template>
