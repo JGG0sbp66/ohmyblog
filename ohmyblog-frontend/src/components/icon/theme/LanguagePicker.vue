3b434b1a4a83ea8868d56a6b62a6ff21e519c41b
JGG0sbp66 <2791456537@qq.com>
2026-01-09 21:38:44 +0800

    feat: 优化 LanguagePicker 组件，调整图标大小

--- ohmyblog-frontend/src/components/icon/theme/LanguagePicker.vue
+++ ohmyblog-frontend/src/components/icon/theme/LanguagePicker.vue
@@ -1,12 +1,13 @@
+<!-- src/components/icon/theme/LanguagePicker.vue -->
 <script lang="ts" setup>
-import { RiTranslate, RiEmphasisCn, RiEnglishInput } from '@remixicon/vue';
+import { RiEmphasisCn, RiEnglishInput } from '@remixicon/vue';
 const props = defineProps<{
     language: string
 }>()
 </script>
 
 <template>
-    <RiTranslate v-if="props.language == 'translate'" class="text-[1.25rem] w-[1em] h-[1em]" />
-    <RiEmphasisCn v-if="props.language == 'zh-CN'" class="text-[1.25rem] w-[1em] h-[1em]" />
-    <RiEnglishInput v-if="props.language == 'en-US'" class="text-[1.25rem] w-[1em] h-[1em]" />
+     <RiTranslate v-if="props.language == 'translate'" class="w-5 h-5" />
+    <RiEmphasisCn v-if="props.language == 'zh-CN'" class="w-5 h-5" />
+    <RiEnglishInput v-if="props.language == 'en-US'" class="w-5 h-5" />
 </template>
