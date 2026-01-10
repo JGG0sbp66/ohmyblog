dbdaeb9ca16cd49b3e9a0e8d4af9969db3a5fec7
JGG0sbp66 <2791456537@qq.com>
2026-01-10 08:49:04 +0800

    feat: 添加 Question 组件，使用 lucide-vue-next 图标

---
+++ ohmyblog-frontend/src/components/icon/Question.vue
@@ -0,0 +1,8 @@
+<!-- src/components/icon/Question.vue -->
+<script setup lang="ts">
+import { CircleQuestionMark } from 'lucide-vue-next';
+</script>
+
+<template>
+    <CircleQuestionMark class="w-5 h-5" />
+</template>
