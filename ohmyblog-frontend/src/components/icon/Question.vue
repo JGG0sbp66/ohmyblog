0708ed04758845148b10d22d08c68f43ede2ec9a
JGG0sbp66 <2791456537@qq.com>
2026-01-10 17:56:36 +0800

    feat: 更新 Question 组件，支持自定义大小类

--- ohmyblog-frontend/src/components/icon/Question.vue
+++ ohmyblog-frontend/src/components/icon/Question.vue
@@ -1,8 +1,13 @@
 <!-- src/components/icon/Question.vue -->
 <script setup lang="ts">
 import { CircleQuestionMark } from 'lucide-vue-next';
+const props = withDefaults(defineProps<{
+    sizeClass?: string;
+}>(), {
+    sizeClass: 'w-4 h-4',
+});
 </script>
 
 <template>
-    <CircleQuestionMark class="w-5 h-5" />
+    <CircleQuestionMark :class="props.sizeClass" />
 </template>
