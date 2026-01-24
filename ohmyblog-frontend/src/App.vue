690f0357e7b5bc06de06e78665078c5147541659
JGG0sbp66 <2791456537@qq.com>
2026-01-24 15:45:29 +0800

    fix: 更新 favicon 逻辑，避免每次请求导致浏览器重新下载图标

--- ohmyblog-frontend/src/App.vue
+++ ohmyblog-frontend/src/App.vue
@@ -9,11 +9,15 @@ onMounted(() => {
   // 初始化主题配置，从服务器同步外观设置
   initThemeConfig();
 
-  const link = document.getElementById('dynamic-favicon') as HTMLLinkElement
-  if (link) {
-    // Vue 启动后，立即给它打上时间戳补丁
-    link.href = `/api/uploads/system/favicon.png?v=${Date.now()}`
-  }
+  // TODO: 逻辑应该修改为
+  // 1. 当管理员修改了网站的icon，才去更新 favicon 链接
+  // 2. 防止每次请求都会导致浏览器重新下载icon，产生性能问题
+
+  // const link = document.getElementById('dynamic-favicon') as HTMLLinkElement
+  // if (link) {
+  //   // Vue 启动后，立即给它打上时间戳补丁
+  //   link.href = `/api/uploads/system/favicon.png?v=${Date.now()}`
+  // }
 });
 </script>
 
