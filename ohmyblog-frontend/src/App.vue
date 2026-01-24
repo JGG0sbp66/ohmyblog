006a5f692bed2c96504597091326b44805ea3527
JGG0sbp66 <2791456537@qq.com>
2026-01-24 19:23:10 +0800

    feat: 移除关于 favicon 更新的 TODO 注释，优化代码可读性

--- ohmyblog-frontend/src/App.vue
+++ ohmyblog-frontend/src/App.vue
@@ -13,16 +13,6 @@ onMounted(() => {
 
   // 获取站点基本信息（如标题等）
   systemStore.fetchSiteInfo();
-
-  // TODO: 逻辑应该修改为
-  // 1. 当管理员修改了网站的icon，才去更新 favicon 链接
-  // 2. 防止每次请求都会导致浏览器重新下载icon，产生性能问题
-
-  // const link = document.getElementById('dynamic-favicon') as HTMLLinkElement
-  // if (link) {
-  //   // Vue 启动后，立即给它打上时间戳补丁
-  //   link.href = `/api/uploads/system/favicon.png?v=${Date.now()}`
-  // }
 });
 </script>
 
