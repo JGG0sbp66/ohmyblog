726d0630b5a91cdfc8f880aabe4e0c27dedad742
JGG0sbp66 <2791456537@qq.com>
2026-01-31 19:58:08 +0800

    feat: 添加上传 DTO，支持网站图标、管理员头像和首页横幅文件上传

--- ohmyblog-backend/src/dtos/config.dto.ts
+++ ohmyblog-backend/src/dtos/config.dto.ts
@@ -28,37 +28,4 @@ export const ConfigUpsertDTO = t.Object({
 	),
 });
 
-// 网站icon图标 DTO
-export const UploadIconDTO = t.Object({
-	icon: t.File({
-		type: "image",
-		maxSize: "1m",
-		description: "网站图标文件，最大1MB",
-		error: "请上传 1MB 以内的有效图片",
-	}),
-});
-
-// 首页 Hero 横幅 DTO
-export const UploadHeroDTO = t.Object({
-	hero: t.File({
-		type: "image",
-		maxSize: "5m",
-		description: "首页横幅图片，最大5MB",
-		error: "请上传 5MB 以内的有效图片",
-	}),
-});
-
-// 管理员头像 DTO
-export const UploadAvatarDTO = t.Object({
-	avatar: t.File({
-		type: "image",
-		maxSize: "2m",
-		description: "管理员头像，最大2MB",
-		error: "请上传 2MB 以内的有效图片",
-	}),
-});
-
 export type TConfigUpsertDTO = Static<typeof ConfigUpsertDTO>;
-export type TUploadIconDTO = Static<typeof UploadIconDTO>;
-export type TUploadHeroDTO = Static<typeof UploadHeroDTO>;
-export type TUploadAvatarDTO = Static<typeof UploadAvatarDTO>;
