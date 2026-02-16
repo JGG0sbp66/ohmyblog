64ea2932cb1c63c11c345ce6b9030fd8f52b975b
JGG0sbp66 <2791456537@qq.com>
2026-02-16 16:57:59 +0800

    BaseTag新增错误tag

---
+++ ohmyblog-frontend/src/components/icon/XCircle.vue
@@ -0,0 +1,16 @@
+<!-- src/components/icon/XCircle.vue -->
+<script setup lang="ts">
+import { XCircle } from "lucide-vue-next";
+const props = withDefaults(
+  defineProps<{
+    sizeClass?: string;
+  }>(),
+  {
+    sizeClass: "w-4 h-4",
+  },
+);
+</script>
+
+<template>
+  <XCircle :class="props.sizeClass" />
+</template>
