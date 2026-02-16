bf5639c0bed93ba4694629204858dbe3eed0b8d6
JGG0sbp66 <2791456537@qq.com>
2026-02-16 08:48:38 +0800

    新增首页组件，包含头部和欢迎信息

---
+++ ohmyblog-frontend/src/views/home/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import Header from "@/components/common/layout/Header.vue";
+</script>
+
+<template>
+  <Header />
+  <div class="container mx-auto px-4 py-8">
+    <h1 class="text-3xl font-bold">Welcome to OhMyBlog</h1>
+  </div>
+</template>
