64476c223fecc58803bd367540dcbe221d359365
JGG0sbp66 <2791456537@qq.com>
2026-02-23 12:22:14 +0800

    feat(前端): 新增 AdminLayout 组件，初步实现后台管理布局

---
+++ ohmyblog-frontend/src/views/admin/components/AdminLayout.vue
@@ -0,0 +1,10 @@
+<!-- src/views/admin/components/AdminLayout.vue -->
+<script setup lang="ts">
+// TODO: 继续开发后台管理布局组件
+</script>
+
+<template>
+  <div class="p-4">
+    <router-view />
+  </div>
+</template>
