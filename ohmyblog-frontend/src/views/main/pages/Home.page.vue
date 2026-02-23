b2aee339eb30055d9ac099402991784f35a5c5fc
JGG0sbp66 <2791456537@qq.com>
2026-02-23 09:28:37 +0800

    feat(前端): 修改 Home 和 Setup 页面的路径，删除不再使用的 Show 页面

--- ohmyblog-frontend/src/views/main/pages/Home.page.vue
+++ ohmyblog-frontend/src/views/main/pages/Home.page.vue
@@ -1,7 +1,7 @@
-<!-- src/views/main/pages/home/index.vue -->
+<!-- src/views/main/pages/Home.page.vue -->
 <!-- TODO: 后续考虑是否将 main 改为嵌套路由结构（类似 admin），使用 /main 作为父路由，home 等页面作为子路由 -->
 <script lang="ts" setup>
-import MainLayout from "../../components/MainLayout.vue";
+import MainLayout from "../components/MainLayout.vue";
 </script>
 
 <template>
--- ohmyblog-frontend/src/views/show/index.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup>
-import Header from "@/components/common/layout/Header.vue";
-</script>
-
-<template>
-  <Header />
-</template>
