31828dd0799911bd92187cb277b346d9d793911c
JGG0sbp66 <2791456537@qq.com>
2026-04-19 20:46:50 +0800

    feat(前端): 添加UI图标组件
    - 添加Bell通知图标组件，使用RiNotification3Line图标
    - 添加Home主页图标组件，使用RiHome5Line图标
    - 两个组件均采用统一的5x5尺寸样式
    - 为管理员后台界面提供基础图标支持

---
+++ ohmyblog-frontend/src/components/icon/ui/Home.vue
@@ -0,0 +1,8 @@
+<!-- src/components/icon/ui/Home.vue -->
+<script setup lang="ts">
+import { RiHome5Line } from "@remixicon/vue";
+</script>
+
+<template>
+  <RiHome5Line class="w-5 h-5" />
+</template>
