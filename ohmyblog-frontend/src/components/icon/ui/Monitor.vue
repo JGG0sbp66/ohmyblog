c57d65bd765f0054e667f380f11c70ca5d044a9d
JGG0sbp66 <2791456537@qq.com>
2026-04-21 15:09:54 +0800

    feat(图标): 添加监视器和智能手机图标组件

---
+++ ohmyblog-frontend/src/components/icon/ui/Monitor.vue
@@ -0,0 +1,8 @@
+<!-- src/components/icon/ui/Monitor.vue -->
+<script setup lang="ts">
+import { RiComputerLine } from "@remixicon/vue";
+</script>
+
+<template>
+  <RiComputerLine />
+</template>
---
+++ ohmyblog-frontend/src/components/icon/ui/Smartphone.vue
@@ -0,0 +1,8 @@
+<!-- src/components/icon/ui/Smartphone.vue -->
+<script setup lang="ts">
+import { RiSmartphoneLine } from "@remixicon/vue";
+</script>
+
+<template>
+  <RiSmartphoneLine />
+</template>
