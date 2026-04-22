f6b0f3742d2baeaa0e5c3a25e0b5b5b2b47cbc64
JGG0sbp66 <2791456537@qq.com>
2026-04-22 10:13:46 +0800

    feat(前端): 新增语言选择器和主题模式选择器组件，优化用户界面和交互体验

---
+++ ohmyblog-frontend/src/views/setup/components/theme/ThemeColorPicker.vue
@@ -0,0 +1,13 @@
+<!-- src/components/theme/ThemeColorPicker.vue -->
+<script setup lang="ts">
+import ColorSlider from "@/components/base/slider/ColorSlider.vue";
+import { useTheme } from "@/composables/theme.hook";
+
+const { currentHue } = useTheme();
+</script>
+
+<template>
+  <div class="h-12 bg-bg-muted rounded-xl flex items-center px-4">
+    <ColorSlider v-model="currentHue" />
+  </div>
+</template>
