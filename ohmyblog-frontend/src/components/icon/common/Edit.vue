89b49a7e67f4a27579188bf235e6f79c748c4c3d
JGG0sbp66 <2791456537@qq.com>
2026-04-22 15:46:51 +0800

    feat(前端): 添加Edit、Heading和Text组件，支持可自定义大小类

---
+++ ohmyblog-frontend/src/components/icon/common/Edit.vue
@@ -0,0 +1,16 @@
+<!-- src/components/icon/common/Edit.vue -->
+<script setup lang="ts">
+import { FilePenLine } from "lucide-vue-next";
+const props = withDefaults(
+  defineProps<{
+    sizeClass?: string;
+  }>(),
+  {
+    sizeClass: "w-4 h-4",
+  },
+);
+</script>
+
+<template>
+  <FilePenLine :class="props.sizeClass" />
+</template>
