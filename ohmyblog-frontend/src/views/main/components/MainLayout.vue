416f10dfdf91186f9d2d205804f3ae0a79524201
JGG0sbp66 <2791456537@qq.com>
2026-04-22 11:25:30 +0800

    feat(前端): 在主布局中添加HeroSection组件

--- ohmyblog-frontend/src/views/main/components/MainLayout.vue
+++ ohmyblog-frontend/src/views/main/components/MainLayout.vue
@@ -2,11 +2,13 @@
 <script setup lang="ts">
 import Header from "@/components/common/layout/Header.vue";
 import Footer from "@/components/common/layout/Footer.vue";
+import HeroSection from "@/views/main/components/HeroSection.vue";
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col">
     <Header />
+    <HeroSection />
     <main class="flex-1">
       <router-view />
     </main>
