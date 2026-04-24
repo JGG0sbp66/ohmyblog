9796a85f7b97519c20354877a1cd32f09796e831
JGG0sbp66 <2791456537@qq.com>
2026-04-24 10:42:33 +0800

    feat(前端): 添加ChevronLeft和ChevronRight组件以增强图标功能

---
+++ ohmyblog-frontend/src/components/icon/common/ChevronRight.vue
@@ -0,0 +1,17 @@
+<!-- src/components/icon/common/ChevronRight.vue -->
+<script setup lang="ts">
+import { RiArrowRightSLine } from "@remixicon/vue";
+
+const props = withDefaults(
+  defineProps<{
+    sizeClass?: string;
+  }>(),
+  {
+    sizeClass: "w-5 h-5",
+  },
+);
+</script>
+
+<template>
+  <RiArrowRightSLine :class="props.sizeClass" />
+</template>
