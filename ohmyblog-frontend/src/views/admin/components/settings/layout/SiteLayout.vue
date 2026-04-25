a3141a139a01c02016c5bcf8ee26e1949717be7c
JGG0sbp66 <2791456537@qq.com>
2026-04-25 21:00:05 +0800

    style(SiteLayout)：调整内边距与间距以实现底部对齐
    - 在 template 开始标签后添加空行以保持代码一致性
    - 将 py-1 pr-1 替换为 pb-8 pl-3 pr-4，修复底部对齐问题
    - 添加 TODO 注释，说明当前内边距会导致底部对齐异常
    - 优化右侧边栏表单容器的间距与滚动条位置

--- ohmyblog-frontend/src/views/admin/components/settings/layout/SiteLayout.vue
+++ ohmyblog-frontend/src/views/admin/components/settings/layout/SiteLayout.vue
@@ -6,14 +6,16 @@ import SiteInfoForm from "../site/SiteInfoForm.vue";
 </script>
 
 <template>
+
   <div class="flex flex-col lg:flex-row gap-8 onload-animation min-h-0 flex-1">
     <!-- 左侧：实时预览区域 -->
     <div class="flex-1 flex flex-col gap-6 min-h-0">
       <SitePreview class="flex-1" />
     </div>
 
+    <!-- TODO: 现在会导致底部对不齐了，因为有这个padding -->
     <!-- 右侧：配置表单区域 -->
-    <div class="flex flex-col gap-6 shrink-0 h-full overflow-y-auto py-1 pr-1">
+    <div class="flex flex-col gap-6 shrink-0 h-full overflow-y-auto pb-8 pl-3 pr-4">
       <!-- 站点信息表单 -->
       <SiteInfoForm class="onload-animation anim-delay-50" />
 
